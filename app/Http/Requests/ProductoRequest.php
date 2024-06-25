1a89a8633d1458470fec788387d4f0b6c522e2ce
Luis Acosta <gitmeleandev.phantom644@passinbox.com>
2024-06-25 18:18:54 -0400

    request

--- app/Http/Requests/ProductoRequest.php
+++ app/Http/Requests/ProductoRequest.php
@@ -11,7 +11,7 @@ class ProductoRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,10 @@ class ProductoRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'nombre' => ['required', 'string', 'min:3', 'max:255'],
+            'descripcion' => ['required', 'string', 'min:3', 'max:255'],
+            'precio' => ['required', 'integer'],
+            'disponible' => ['required', 'integer']
         ];
     }
 }
