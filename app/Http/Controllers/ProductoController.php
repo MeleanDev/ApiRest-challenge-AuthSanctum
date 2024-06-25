b72520f39d16b9495b9deb685f891b380ae15592
Luis Acosta <gitmeleandev.phantom644@passinbox.com>
2024-06-25 19:05:48 -0400

    mover archivo productoCLass a services

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ProductoRequest;
 use App\Models\Producto;
-use App\ProductoClass;
+use App\Service\ProductoClass;
 use Illuminate\Http\Request;
 
 class ProductoController extends Controller
